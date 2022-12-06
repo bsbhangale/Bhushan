3ed34ef06ccfbc20168944719b9c4c611a1e25ae
bsbhangale <119921299+bsbhangale@users.noreply.github.com>
2022-12-06 15:19:14 +0530

    My First Commit

---
+++ First Project/src/Hello_world.java
@@ -0,0 +1,22 @@
+import javax.sound.midi.Soundbank;
+
+public class Hello_world {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world ");
+		System.out.print("Hello");
+		System.out.print("World");
+		System.out.print("Java");
+		System.out.println("Hello world ");
+		System.out.println("Hello Java ");
+		System.out.println("Hello bhushan ");
+		int a=4;
+		int b=5;
+		int c=++a;
+		int d=a++;
+		System.out.println(c);
+		System.out.println(d);
+	}
+
+}
